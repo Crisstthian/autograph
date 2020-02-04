0eb190894e7500237bec23a642fa9bebca4c40a5
Crisstthian <escipion25@gmail.com>
2020-02-04 00:44:17 -0500

    working on xml sign

--- src/com/abianic/Autograph.java
+++ src/com/abianic/Autograph.java
@@ -9,20 +9,18 @@ public class Autograph {
         String nameFileOut = args[3];
         String xmlPath = args[4];
 
-        //xmlPath = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><factura  id=\"comprobante\" version=\"1.0.0\"><infoTributaria><ambiente>1</ambiente></infoTributaria><infoFactura><fechaEmision>01/01/2000</fechaEmision><totalConImpuestos><totalImpuesto><codigo>2</codigo></totalImpuesto></totalConImpuestos><pagos><pago><formaPago>01</formaPago></pago></pagos></infoFactura><detalles><detalle><codigoPrincipal>codigoPrincipal0</codigoPrincipal><detallesAdicionales><detAdicional nombre=\"nombre0\" valor=\"valor0\"/></detallesAdicionales><impuestos><impuesto><codigo>2</codigo></impuesto></impuestos></detalle></detalles><infoAdicional><campoAdicional nombre=\"nombre4\">campoAdicional0</campoAdicional></infoAdicional></factura>";
-
         System.out.println("Ruta del XML de entrada: " + xmlPath);
-        //System.out.println("Ruta Certificado: " + pathSignature);
-        //System.out.println("Clave del Certificado: " + passSignature);
-        //System.out.println("Ruta de salida del XML: " + pathOut);
-        //System.out.println("Nombre del archivo salido: " + nameFileOut);
+        System.out.println("Ruta Certificado: " + pathSignature);
+        System.out.println("Clave del Certificado: " + passSignature);
+        System.out.println("Ruta de salida del XML: " + pathOut);
+        System.out.println("Nombre del archivo salido: " + nameFileOut);
 
-        XAdESBESStringSignature famoso = new XAdESBESStringSignature(xmlPath);
+        XAdESBESFileSignature famoso = new XAdESBESFileSignature(xmlPath);
 
         String resul = null;
 
         try {
-            resul = famoso.firmarString(xmlPath, pathSignature, passSignature, pathOut, nameFileOut);
+            resul = famoso.firmarFile(xmlPath, pathSignature, passSignature, pathOut, nameFileOut);
         } catch (Exception e) {
             e.printStackTrace();
         }

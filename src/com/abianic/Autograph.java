59a32f226bdc97518e5a282895c8bb703502c266
Cristhian Garcia <cristhian.garcia@segurossucre.fin.ec>
2020-02-03 19:15:24 -0500

    working on xml sign

--- src/com/abianic/Autograph.java
+++ src/com/abianic/Autograph.java
@@ -5,9 +5,9 @@ public class Autograph {
     public static void main(String[] args) {
         String pathSignature = args[0];
         String passSignature = args[1];
-        String xmlPath = args[2];
-        String pathOut = args[3];
-        String nameFileOut = args[4];
+        String pathOut = args[2];
+        String nameFileOut = args[3];
+        String xmlPath = args[4];
 
         //xmlPath = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><factura  id=\"comprobante\" version=\"1.0.0\"><infoTributaria><ambiente>1</ambiente></infoTributaria><infoFactura><fechaEmision>01/01/2000</fechaEmision><totalConImpuestos><totalImpuesto><codigo>2</codigo></totalImpuesto></totalConImpuestos><pagos><pago><formaPago>01</formaPago></pago></pagos></infoFactura><detalles><detalle><codigoPrincipal>codigoPrincipal0</codigoPrincipal><detallesAdicionales><detAdicional nombre=\"nombre0\" valor=\"valor0\"/></detallesAdicionales><impuestos><impuesto><codigo>2</codigo></impuesto></impuestos></detalle></detalles><infoAdicional><campoAdicional nombre=\"nombre4\">campoAdicional0</campoAdicional></infoAdicional></factura>";
 
